ccbae995016a11533a0cb83b380214e7916ada25
NavarroAaron <424004090@ntc.edu.ph>
2025-12-09 00:00:11 +0800

    Update main.cpp

--- NepoCoders_SDG2_DSA/CODE/main.cpp
+++ NepoCoders_SDG2_DSA/CODE/main.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
 #include "FamilyModule.h"
 #include "FoodPackModule.h"
 #include "QueueModule.h"
 #include "DistributionModule.h"
 #include "SearchModule.h"
+#include <iostream>
+using namespace std;
 
 int main() {
     LoadSampleData(); 
-
     int choice;
     do {
-        std::cout << "\n===== DISTRICT AID SYSTEM =====\n";
-        std::cout << "1. Register Family\n";
-        std::cout << "2. Add Food Packs\n";
-        std::cout << "3. Distribution of Food Pack\n";
-        std::cout << "4. Search Family\n";
-        std::cout << "5. Show Families\n";
-        std::cout << "6. Show Food Packs\n";
-        std::cout << "7. Show Queue\n";
-        std::cout << "8. Exit\n";
-        std::cout << "Enter choice: ";
-        std::cin >> choice;
+        cout << "\n===== DISTRICT AID SYSTEM =====\n";
+        cout << "1. Register Family\n";
+        cout << "2. Add Food Packs\n";
+        cout << "3. Distribution of Food Pack\n";
+        cout << "4. Search Family\n";
+        cout << "5. Show Families\n";
+        cout << "6. Show Food Packs\n";
+        cout << "7. Show Queue\n";
+        cout << "8. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
 
         switch(choice) {
             case 1: RegisterFamily(); break;
@@ -30,8 +30,8 @@ int main() {
             case 5: DisplayFamilies(); break;
             case 6: DisplayFood(); break;
             case 7: DisplayQueue(); break;
-            case 8: std::cout << "Thank you for helping our society <3.\n"; break;
-            default: std::cout << "Invalid choice!\n";
+            case 8: cout << "Thank you for helping our society <3.\n"; break;
+            default: cout << "Invalid choice!\n";
         }
     } while(choice != 8);
 
