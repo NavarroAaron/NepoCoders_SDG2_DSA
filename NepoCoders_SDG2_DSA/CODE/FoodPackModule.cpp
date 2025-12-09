f77551562582b9af616f745e8c457a0048a113c1
yong <424003394@ntc.edu.ph>
2025-12-09 20:02:42 +0800

    Update FoodPackModule.cpp

--- NepoCoders_SDG2_DSA/CODE/FoodPackModule.cpp
+++ NepoCoders_SDG2_DSA/CODE/FoodPackModule.cpp
@@ -1,3 +1,42 @@
 #include "FoodPackModule.h"
+#include <iostream>
+using namespace std;
 
-// Justine's Food Pack Management implementation goes here
+vector<pair<string, int>> FoodPacks;
+
+void AddFoodPack() {
+    string foodName;
+    int qty;
+    cout << "\nEnter Food Pack name: ";
+    cin >> foodName;
+    cout << "How many units are you adding? ";
+    cin >> qty;
+
+    bool found = false;
+    for (auto &f : FoodPacks) {
+        if (f.first == foodName) {
+            f.second += qty;
+            found = true;
+            cout << "Updated " << foodName << " quantity: " << f.second << endl;
+            break;
+        }
+    }
+
+    if (!found) {
+        FoodPacks.push_back({foodName, qty});
+        cout << "Added " << foodName << " with quantity: " << qty << endl;
+    }
+}
+
+void DisplayFood() {
+    cout << "\nFOOD INVENTORY:\n";
+    if (FoodPacks.empty()) { cout << "No food packs left.\n"; return; }
+
+    for (auto &f : FoodPacks) {
+        cout << "- " << f.first << " = " << f.second << endl;
+    }
+}
+
+
+
+
