f0206a9be118007011e122cfe33335f15acabad5
itscristx <424003153@ntc.edu.ph>
2025-12-09 20:33:03 +0800

    Update FamilyModule.h

--- NepoCoders_SDG2_DSA/CODE/FamilyModule.h
+++ NepoCoders_SDG2_DSA/CODE/FamilyModule.h
@@ -1,6 +1,26 @@
 #ifndef FAMILYMODULE_H
 #define FAMILYMODULE_H
+#ifndef FAMILYMODULE_H
+#define FAMILYMODULE_H
 
-// Camban's Family Registration code goes here
+#include <string>
+#include <vector>
+#include <queue>
+
+struct Family {
+    int Id;
+    std::string Name;
+    int Members;
+    std::string Address;
+    bool HasReceivedFood;
+    char Type;
+};
+
+extern std::vector<Family> Families;
+extern std::queue<Family> AidQueue;
+
+bool IsDuplicate(std::string name);
+bool IsPriority(char type);
+void RegisterFamily();
 
 #endif
