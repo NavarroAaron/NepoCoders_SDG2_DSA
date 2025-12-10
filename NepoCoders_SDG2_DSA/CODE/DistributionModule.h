220cd38fffb3008352a3795995f4fbc716ce50d2
NavarroAaron <424004090@ntc.edu.ph>
2025-12-10 12:18:28 +0800

    Update DistributionModule.h

--- NepoCoders_SDG2_DSA/CODE/DistributionModule.h
+++ NepoCoders_SDG2_DSA/CODE/DistributionModule.h
@@ -1,12 +1,10 @@
 #ifndef DISTRIBUTIONMODULE_H
 #define DISTRIBUTIONMODULE_H
 
-#ifndef DISTRIBUTIONMODULE_H
-#define DISTRIBUTIONMODULE_H
-
 #include <string>
 
 void RemoveFamilyFromList(std::string name);
 void Distribute();
 
 #endif
+
